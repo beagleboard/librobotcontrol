3878a183eef0d0c7aefbe582302150e48bf9b838
StrawsonDesign <jstrawso@gmail.com>
2016-01-18 15:22:05 -0800

    simple_pwm can now change pwm freq on older images

--- libraries/simple_pwm/simple_pwm.c
+++ libraries/simple_pwm/simple_pwm.c
@@ -43,7 +43,7 @@ int period_ns[3]; 	//one period (frequency) per subsystem
 char simple_pwm_initialized[3] = {0,0,0};
 
 int simple_init_pwm(int subsystem, int frequency){
-	int fd, len;
+	int export_fd, len;
 	char buf[MAXBUF];
 	DIR* dir;
 	int periodA_fd; // pointers to frequency file pointer
@@ -58,17 +58,17 @@ int simple_init_pwm(int subsystem, int frequency){
 		return -1;
 	}
 	
-	fd = open(SYSFS_PWM_DIR "/export", O_WRONLY);
-	if (fd < 0) {
+	// unexport the channels first
+	simple_uninit_pwm(subsystem);
+	
+	export_fd = open(SYSFS_PWM_DIR "/export", O_WRONLY);
+	if (export_fd < 0) {
 		printf("error opening pwm export file\n");
 		return -1;
 	}
-	// export the two pwm channels for that subsystem
+	// export just the A channel for that subsystem
 	len = snprintf(buf, sizeof(buf), "%d", 2*subsystem);
-	write(fd, buf, len);
-	len = snprintf(buf, sizeof(buf), "%d", (2*subsystem)+1);
-	write(fd, buf, len);
-	close(fd);
+	write(export_fd, buf, len);
 	
 	//check that the right pwm directories were created
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/", 2*subsystem);
@@ -78,6 +78,35 @@ int simple_init_pwm(int subsystem, int frequency){
 		printf("failed to export pwm%d\n",2*subsystem);
 		return -1;
 	}
+	
+	// set up file descriptors for A channel
+	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/run", (2*subsystem));
+	runA_fd = open(buf, O_WRONLY);
+	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/period_ns", (2*subsystem));
+	periodA_fd = open(buf, O_WRONLY);
+	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/duty_ns", (2*subsystem));
+	duty_fd[(2*subsystem)] = open(buf, O_WRONLY);
+	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/polarity", (2*subsystem)+1);
+	polarityA_fd = open(buf, O_WRONLY);
+
+	
+	// disable A channel and set polarity before setting frequency
+	write(runA_fd, "0", 1);
+	write(duty_fd[(2*subsystem)], "0", 1); // set duty cycle to 0
+	write(polarityA_fd, "0", 1); // set the polarity
+
+	// set the period in nanoseconds
+	period_ns[subsystem] = 1000000000/frequency;
+	len = snprintf(buf, sizeof(buf), "%d", period_ns[subsystem]);
+	write(periodA_fd, buf, len);
+
+	
+	// now we can set up the 'B' channel since the period has been set
+	// the driver will not let you change the period when both are exported
+	
+	// export the B channel
+	len = snprintf(buf, sizeof(buf), "%d", (2*subsystem)+1);
+	write(export_fd, buf, len);
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/", (2*subsystem)+1);
 	dir = opendir(buf);
 	if (dir!=NULL) closedir(dir); //success
@@ -85,59 +114,37 @@ int simple_init_pwm(int subsystem, int frequency){
 		printf("failed to export pwm%d\n",(2*subsystem)+1);
 		return -1;
 	}
-	
-	// set up run file descriptors
-	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/run", (2*subsystem));
-	runA_fd = open(buf, O_WRONLY);
+	// set up file descriptors for B channel
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/run", (2*subsystem)+1);
 	runB_fd = open(buf, O_WRONLY);
-	
-	// set up period file descriptors
-	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/period_ns", (2*subsystem));
-	periodA_fd = open(buf, O_WRONLY);
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/period_ns", (2*subsystem)+1);
 	periodB_fd = open(buf, O_WRONLY);
-	
-	// set up duty file descriptors
-	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/duty_ns", (2*subsystem));
-	duty_fd[(2*subsystem)] = open(buf, O_WRONLY);
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/duty_ns", (2*subsystem)+1);
 	duty_fd[(2*subsystem)+1] = open(buf, O_WRONLY);
-	
-	// set up polarity file descriptors
-	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/polarity", (2*subsystem)+1);
-	polarityA_fd = open(buf, O_WRONLY);
 	len = snprintf(buf, sizeof(buf), SYSFS_PWM_DIR "/pwm%d/polarity", (2*subsystem)+1);
 	polarityB_fd = open(buf, O_WRONLY);
 	
-	
-	// disable A & B channels before setting frequency
-	write(runA_fd, "0", 1);
+	// disable the run value and set polarity before period
 	write(runB_fd, "0", 1);
-	
-	// set duty cycle to 0
-	write(duty_fd[(2*subsystem)], "0", 1);
+	write(polarityB_fd, "0", 1);
 	write(duty_fd[(2*subsystem)+1], "0", 1);
 	
-	// set the polarity
-	write(polarityA_fd, "0", 1);
-	write(polarityB_fd, "0", 1);
-	close(polarityA_fd);
-	close(polarityB_fd);
-	
-	// set the period in nanoseconds
-	period_ns[subsystem] = 1000000000/frequency;
+	// set the period to match the A channel
 	len = snprintf(buf, sizeof(buf), "%d", period_ns[subsystem]);
-	write(periodA_fd, buf, len);
 	write(periodB_fd, buf, len);
-	close(periodA_fd);
-	close(periodB_fd);
 	
 	// enable A&B channels
 	write(runA_fd, "1", 1);
 	write(runB_fd, "1", 1);
+	
+	// close all the files
+	close(export_fd);
 	close(runA_fd);
 	close(runB_fd);
+	close(periodA_fd);
+	close(periodB_fd);
+	close(polarityA_fd);
+	close(polarityB_fd);
 	
 	// everything successful
 	simple_pwm_initialized[subsystem] = 1;
