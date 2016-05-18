bf2bbe80382e76447cce4b74e4df4c6c17d2807f
StrawsonDesign <jstrawso@gmail.com>
2016-05-18 00:21:32 -0700

    delete everything for refresh

--- install_files/pru/make_pru_bins.sh
+++
@@ -1,6 +0,0 @@
-pasm -b pru_0_encoder.p
-pasm -b pru_1_servo.p
-
-cp pru_0_encoder.bin /usr/bin
-cp pru_1_servo.bin /usr/bin
-
--- libraries/simple_pwm/simple_pwm.h
+++
@@ -1,8 +0,0 @@
-// C library for interfacing with the TI PWM userspace driver
-// This relies on a device tree overlay enabling hrpwm 
-
-
-int simple_init_pwm(int subsystem, int frequency);
-int simple_uninit_pwm(int subsystem);
-int simple_set_pwm_duty(int subsystem, char ch, float duty);
-int simple_set_pwm_duty_ns(int subsystem, char ch, int duty_ns);
