6e23e569d28be34e0b43305465366f2fa1532cf8
root <root@beaglebone.localdomain>
2016-09-18 08:18:10 +0000

    change 'subsystem' variable to 'ss' to be consistent with eqep code

--- libraries/simple_pwm/simple_pwm.h
+++ libraries/simple_pwm/simple_pwm.h
@@ -2,7 +2,7 @@
 // This relies on a device tree overlay enabling hrpwm 
 
 
-int simple_init_pwm(int subsystem, int frequency);
-int simple_uninit_pwm(int subsystem);
-int simple_set_pwm_duty(int subsystem, char ch, float duty);
-int simple_set_pwm_duty_ns(int subsystem, char ch, int duty_ns);
+int simple_init_pwm(int ss, int frequency);
+int simple_uninit_pwm(int ss);
+int simple_set_pwm_duty(int ss, char ch, float duty);
+int simple_set_pwm_duty_ns(int ss, char ch, int duty_ns);
