2fb1806363a4dd3a63099e4d5661c1c783a3fe0f
root <root@beaglebone.localdomain>
2016-10-01 06:43:27 +0000

    support both old and new ti pwm drivers

--- libraries/simple_pwm/simple_pwm.c
+++ libraries/simple_pwm/simple_pwm.c
@@ -14,8 +14,13 @@
 int duty_fd[6]; 	// pointers to duty cycle file descriptor
 int period_ns[3]; 	//one period (frequency) per subsystem
 char simple_pwm_initialized[3] = {0,0,0};
+int ver; // pwm driver version, 0 or 1. automatically detected
 
-//
+/*******************************************************************************
+* int simple_init_pwm(int ss, int frequency)
+*
+* configures subsystem 0, 1, or 2 to operate at a particular frequency.
+*******************************************************************************/
 int simple_init_pwm(int ss, int frequency){
 	int export_fd;
 	int periodA_fd; // pointers to frequency file pointer
@@ -33,13 +38,15 @@ int simple_init_pwm(int ss, int frequency){
 	}
 	
 	// check driver is loaded
-	if(access(pwm_export_path[ss], F_OK ) != 0){
-		printf("ERROR: ti-pwm driver not loaded for hrpwm%d\n", ss);
+	if(access(pwm_export_path[0][ss], F_OK ) == 0) ver=0;
+	else if(access(pwm_export_path[1][ss], F_OK ) == 0) ver=1;
+	else{
+		printf("ERROR: ti-pwm driver not loaded for pwm subsystem %d\n", ss);
 		return -1;
 	}
 		
 	// open export file for that subsystem
-	export_fd = open(pwm_export_path[ss], O_WRONLY);
+	export_fd = open(pwm_export_path[ver][ss], O_WRONLY);
 	if (export_fd < 0) {
 		printf("error opening pwm export file for writing\n");
 		return -1;
@@ -47,16 +54,16 @@ int simple_init_pwm(int ss, int frequency){
 
 	// export just the A channel for that subsystem and check that it worked
 	write(export_fd, "0", 1); 
-	if(access(pwm_chA_enable_path[ss], F_OK ) != 0){
+	if(access(pwm_chA_enable_path[ver][ss], F_OK ) != 0){
 		printf("ERROR: export failed for hrpwm%d channel A\n", ss);
 		return -1;
 	}
 	
 	// set up file descriptors for A channel
-	enableA_fd = open(pwm_chA_enable_path[ss], O_WRONLY);
-	periodA_fd = open(pwm_chA_period_path[ss], O_WRONLY);
-	duty_fd[(2*ss)] = open(pwm_chA_duty_path[ss], O_WRONLY);
-	polarityA_fd = open(pwm_chA_polarity_path[ss], O_WRONLY);
+	enableA_fd = open(pwm_chA_enable_path[ver][ss], O_WRONLY);
+	periodA_fd = open(pwm_chA_period_path[ver][ss], O_WRONLY);
+	duty_fd[(2*ss)] = open(pwm_chA_duty_path[ver][ss], O_WRONLY);
+	polarityA_fd = open(pwm_chA_polarity_path[ver][ss], O_WRONLY);
 
 	
 	// disable A channel and set polarity before period
@@ -75,16 +82,16 @@ int simple_init_pwm(int ss, int frequency){
 	
 	// export the B channel and check that it worked
 	write(export_fd, "1", 1);
-	if(access(pwm_chB_enable_path[ss], F_OK ) != 0){
+	if(access(pwm_chB_enable_path[ver][ss], F_OK ) != 0){
 		printf("ERROR: export failed for hrpwm%d channel B\n", ss);
 		return -1;
 	}
 
 	// set up file descriptors for B channel
-	enableB_fd = open(pwm_chB_enable_path[ss], O_WRONLY);
-	periodB_fd = open(pwm_chB_period_path[ss], O_WRONLY);
-	duty_fd[(2*ss)+1] = open(pwm_chB_duty_path[ss], O_WRONLY);
-	polarityB_fd = open(pwm_chB_polarity_path[ss], O_WRONLY);
+	enableB_fd = open(pwm_chB_enable_path[ver][ss], O_WRONLY);
+	periodB_fd = open(pwm_chB_period_path[ver][ss], O_WRONLY);
+	duty_fd[(2*ss)+1] = open(pwm_chB_duty_path[ver][ss], O_WRONLY);
+	polarityB_fd = open(pwm_chB_polarity_path[ver][ss], O_WRONLY);
 	
 	// disable and set polarity before period
 	write(enableB_fd, "0", 1);
@@ -113,6 +120,11 @@ int simple_init_pwm(int ss, int frequency){
 	return 0;
 }
 
+/*******************************************************************************
+* int simple_uninit_pwm(int ss){
+*
+* unexports a subsystem to put it into low-power state
+*******************************************************************************/
 int simple_uninit_pwm(int ss){
 	int fd;
 
@@ -122,8 +134,16 @@ int simple_uninit_pwm(int ss){
 		return -1;
 	}
 
+	// attempt both driver versions
+	if(access(pwm_unexport_path[0][ss], F_OK ) == 0) ver=0;
+	else if(access(pwm_unexport_path[1][ss], F_OK ) == 0) ver=1;
+	else{
+		printf("ERROR: ti-pwm driver not loaded for pwm subsystem %d\n", ss);
+		return -1;
+	}
+
 	// open the unexport file for that subsystem
-	fd = open(pwm_unexport_path[ss], O_WRONLY);
+	fd = open(pwm_unexport_path[ver][ss], O_WRONLY);
 	if (fd < 0) {
 		printf("error opening pwm unexport file for hrpwm%d\n", ss);
 		return -1;
@@ -139,7 +159,12 @@ int simple_uninit_pwm(int ss){
 	
 }
 
-
+/*******************************************************************************
+* int simple_set_pwm_duty(int ss, char ch, float duty)
+*
+* updates the duty cycle through the file system. see mmap_pwmss.h for a faster
+* method.
+*******************************************************************************/
 int simple_set_pwm_duty(int ss, char ch, float duty){
 	// start with sanity checks
 	if(duty>1.0 || duty<0.0){
@@ -152,6 +177,11 @@ int simple_set_pwm_duty(int ss, char ch, float duty){
 	return simple_set_pwm_duty_ns(ss, ch, duty_ns);
 }
 
+/*******************************************************************************
+* int simple_set_pwm_duty_ns(int ss, char ch, int duty_ns)
+*
+* like simple_set_pwm_duty() but takes a width in nanoseconds.
+*******************************************************************************/
 int simple_set_pwm_duty_ns(int ss, char ch, int duty_ns){
 	int len;
 	char buf[MAXBUF];
--- libraries/simple_pwm/ti_pwm_userspace_defs.h
+++ libraries/simple_pwm/ti_pwm_userspace_defs.h
@@ -5,58 +5,90 @@
 
 
 // export directories
-
-
-const char* pwm_export_path[3] = { \
+const char* pwm_export_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/export", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/export", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/export"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/export"},{ \
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/export", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/export", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/export"}};\
 
-const char* pwm_unexport_path[3] = { \
+
+const char* pwm_unexport_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/unexport", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/unexport", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/unexport"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/unexport"},{ \
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/unexport", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/unexport", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/unexport"}};\
 
 
-const char* pwm_chA_enable_path[3] = { \
+// channel enable
+const char* pwm_chA_enable_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/enable", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm0/enable", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/enable"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/enable"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm0/enable", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm0/enable", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm0/enable"}};
 
-const char* pwm_chB_enable_path[3] = { \
+const char* pwm_chB_enable_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm1/enable", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm1/enable", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/enable"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/enable"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm1/enable", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm1/enable", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm1/enable"}};
 
-
-const char* pwm_chA_polarity_path[3] = { \
+// channel polarity
+const char* pwm_chA_polarity_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/polarity", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm0/polarity", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/polarity"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/polarity"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm0/polarity", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm0/polarity", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm0/polarity"}};
 
-const char* pwm_chB_polarity_path[3] = { \
+const char* pwm_chB_polarity_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm1/polarity", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm1/polarity", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/polarity"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/polarity"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm1/polarity", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm1/polarity", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm1/polarity"}};
 
 
-const char* pwm_chA_period_path[3] = { \
+// channel period
+const char* pwm_chA_period_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/period", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm0/period", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/period"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/period"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm0/period", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm0/period", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm0/period"}};
 
-const char* pwm_chB_period_path[3] = { \
+const char* pwm_chB_period_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm1/period", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm1/period", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/period"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/period"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm1/period", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm1/period", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm1/period"}};
 
-
-const char* pwm_chA_duty_path[3] = { \
+// channel duty cycle
+const char* pwm_chA_duty_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/duty_cycle", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm0/duty_cycle", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/duty_cycle"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm0/duty_cycle"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm0/duty_cycle", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm0/duty_cycle", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm0/duty_cycle"}};
 
-const char* pwm_chB_duty_path[3] = { \
+const char* pwm_chB_duty_path[2][3] = {{ \
 "/sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm1/duty_cycle", \
 "/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/pwmchip2/pwm1/duty_cycle", \
-"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/duty_cycle"};
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.ehrpwm/pwm/pwmchip4/pwm1/duty_cycle"},{
+"/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/pwm/pwmchip0/pwm1/duty_cycle", \
+"/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip2/pwm1/duty_cycle", \
+"/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/pwm/pwmchip4/pwm1/duty_cycle"}};
+
