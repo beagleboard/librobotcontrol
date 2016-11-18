d5a43111b9b36017659a0e57e50a2ac3bf9cfb08
root <root@beaglebone.localdomain>
2016-11-18 10:26:34 +0000

    clean up balance_config.h

--- examples/balance/balance_config.h
+++ examples/balance/balance_config.h
@@ -1,40 +1,36 @@
 /*******************************************************************************
-* balance_config.c
+* balance_config.h
 *
-* Contains the settings struct for configuration of balance.c
-* Settings are contained here for neatness.
-* Macros are used to take CONFIG_TABLE and both declare a struct
-* and then fill it with the default values.
+* Contains the settings for configuration of balance.c
 *******************************************************************************/
 
 #ifndef BALANCE_CONFIG
 #define BALANCE_CONFIG
 
 #define SAMPLE_RATE_HZ 200	// main filter and control loop speed
-#define DT 0.005      		// 1/sample_rate
+#define DT 0.005			// 1/sample_rate
 
 // Structural properties of eduMiP
-//#define 	CAPE_MOUNT_ANGLE		0.40
-#define 	CAPE_MOUNT_ANGLE		0.35 // for blue
-#define 	GEARBOX 				35.577
-#define 	ENCODER_RES				60
-#define 	WHEEL_RADIUS_M			0.034
-#define 	TRACK_WIDTH_M			0.035
-#define 	V_NOMINAL				7.4
+#define CAPE_MOUNT_ANGLE		0.40
+#define GEARBOX 				35.577
+#define ENCODER_RES				60
+#define WHEEL_RADIUS_M			0.034
+#define TRACK_WIDTH_M			0.035
+#define V_NOMINAL				7.4
 
 // inner loop controller 200hz old
-#define 	D1_GAIN					0.8	
-#define 	D1_ORDER				2
-#define 	D1_NUM					{-6.289, 11.910, -5.634 }
-#define 	D1_DEN					{ 1.000, -1.702,  0.702 }
-#define 	D1_SATURATION_TIMEOUT	0.5
+#define D1_GAIN					0.8
+#define D1_ORDER				2
+#define D1_NUM					{-6.289, 11.910, -5.634 }
+#define D1_DEN					{ 1.000, -1.702,  0.702 }
+#define D1_SATURATION_TIMEOUT	0.5
 
 // outer loop controller original 200hz
-#define 	D2_GAIN					0.7
-#define		D2_ORDER				1
-#define 	D2_NUM					{ 0.3858, -0.3853 }
-#define 	D2_DEN					{ 1.0000, -0.9277 }
-#define 	THETA_REF_MAX	0.37
+#define D2_GAIN					0.7
+#define	D2_ORDER				1
+#define D2_NUM					{ 0.3858, -0.3853 }
+#define D2_DEN					{ 1.0000, -0.9277 }
+#define THETA_REF_MAX			0.37
 
 
 // // inner loop controller 100hz new
@@ -53,45 +49,45 @@
 // #define 	THETA_REF_MAX			0.37
 
 // steering controller
-#define 	D3_KP					1.0
-#define 	D3_KI					0.05
-#define 	D3_KD					0.1
-#define		STEERING_INPUT_MAX		0.5
+#define D3_KP					1.0
+#define D3_KI					0.05
+#define D3_KD					0.1
+#define STEERING_INPUT_MAX		0.5
 
 // electrical hookups
-#define 	MOTOR_CHANNEL_L 		3
-#define 	MOTOR_CHANNEL_R 		2
-#define 	MOTOR_POLARITY_L 		1
-#define 	MOTOR_POLARITY_R 		-1
-#define 	ENCODER_CHANNEL_L 		3
-#define 	ENCODER_CHANNEL_R 		2
-#define 	ENCODER_POLARITY_L 		1
-#define 	ENCODER_POLARITY_R 		-1
+#define MOTOR_CHANNEL_L			3
+#define MOTOR_CHANNEL_R			2
+#define MOTOR_POLARITY_L		1
+#define MOTOR_POLARITY_R		-1
+#define ENCODER_CHANNEL_L		3
+#define ENCODER_CHANNEL_R		2
+#define ENCODER_POLARITY_L		1
+#define ENCODER_POLARITY_R		-1
 
 //	drive speeds when using remote control (dsm2)
-#define 	DRIVE_RATE_NOVICE 		16
-#define 	TURN_RATE_NOVICE		6
-#define 	DRIVE_RATE_ADVANCED		26
-#define 	TURN_RATE_ADVANCED		10
+#define DRIVE_RATE_NOVICE		16
+#define TURN_RATE_NOVICE		6
+#define DRIVE_RATE_ADVANCED		26
+#define TURN_RATE_ADVANCED		10
 
 // DSM channel config
-#define 	DSM_DRIVE_POL			1
-#define 	DSM_TURN_POL		  	-1
-#define 	DSM_DRIVE_CH 	 	 	3
-#define 	DSM_TURN_CH 	 	 	2
-#define 	DSM_DEAD_ZONE			0.04
+#define DSM_DRIVE_POL			1
+#define DSM_TURN_POL			-1
+#define DSM_DRIVE_CH			3
+#define DSM_TURN_CH				2
+#define DSM_DEAD_ZONE			0.04
 
 // Thread Loop Rates
-#define		BATTERY_CHECK_HZ		5
-#define 	SETPOINT_MANAGER_HZ		100
-#define		PRINTF_HZ				50
+#define BATTERY_CHECK_HZ		5
+#define SETPOINT_MANAGER_HZ		100
+#define PRINTF_HZ				50
 
 // other
-#define 	TIP_ANGLE 				0.75
-#define 	START_ANGLE 			0.3	
-#define 	START_DELAY 			0.5	
-#define 	PICKUP_DETECTION_TIME 	0.65
-#define 	ENABLE_POSITION_HOLD	1
-#define 	SOFT_START_SEC			0.7
+#define TIP_ANGLE 				0.75
+#define START_ANGLE 			0.3
+#define START_DELAY 			0.5
+#define PICKUP_DETECTION_TIME	0.65
+#define ENABLE_POSITION_HOLD	1
+#define SOFT_START_SEC			0.7
 
-#endif //BALANCE_CONFIG
+#endif	//BALANCE_CONFIG
