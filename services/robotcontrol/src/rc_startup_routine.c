02ec8ca34433cba8e48e125011cd4c4d8958a603
Demo User <debian@beaglebone.localdomain>
2018-11-09 04:02:08 +0000

    make rc_startup_routine more robust to pwm driver versions

--- services/robotcontrol/src/rc_startup_routine.c
+++ services/robotcontrol/src/rc_startup_routine.c
@@ -25,7 +25,7 @@
 
 static int set_gpio_permissions();
 static int check_timeout();
-static int setup_pwm();
+static int check_pwm();
 static int check_eqep();
 
 static uint64_t start_us;
@@ -64,7 +64,7 @@ int main()
 		if(check_timeout()){
 			system("echo 'timeout reached while waiting for gpio driver' >> " START_LOG);
 			fprintf(stderr,"timeout reached while waiting for gpio driver\n");
-			return -1;
+			return 0;
 		}
 		rc_usleep(500000);
 	}
@@ -74,11 +74,11 @@ int main()
 
 
 	// set up pwm at desired frequnecy
-	while(setup_pwm()!=0){
+	while(check_pwm()!=0){
 		if(check_timeout()){
 			system("echo 'timeout reached while waiting for pwm driver' >> " START_LOG);
 			fprintf(stderr,"timeout reached while waiting for pwm driver\n");
-			return -1;
+			return 0;
 		}
 		rc_usleep(500000);
 	}
@@ -91,7 +91,7 @@ int main()
 		if(check_timeout()){
 			system("echo 'timeout reached while waiting for eQEP driver' >> " START_LOG);
 			fprintf(stderr,"timeout reached while waiting for eQEP driver\n");
-		 	return -1;
+		 	return 0;
 		}
 		rc_usleep(500000);
 	}
@@ -161,45 +161,18 @@ int check_timeout()
  *
  * @return     returns 0 on success or -1 on failure
  */
-int setup_pwm()
+int check_pwm()
 {
 	// first make sure the driver is loaded, if so the export paths will appear
-	if(access("/sys/class/pwm/pwmchip0/export",F_OK)!=0){
-		// export directory not there yet, wait a bit more
+	if(access("/sys/devices/platform/ocp/48300000.epwmss/48300200.pwm/",F_OK)!=0){
 		return -1;
 	}
-	if(access("/sys/class/pwm/pwmchip2/export",F_OK)!=0){
-		// export directory not there yet, wait a bit more
+	if(access("/sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/",F_OK)!=0){
 		return -1;
 	}
-	if(access("/sys/class/pwm/pwmchip4/export",F_OK)!=0){
-		// export directory not there yet, wait a bit more
+	if(access("/sys/devices/platform/ocp/48304000.epwmss/48304200.pwm/",F_OK)!=0){
 		return -1;
 	}
-
-	// now export the channels if necessary
-	if(access("/sys/class/pwm/pwmchip0/pwm0/enable",F_OK)!=0){
-		system("/bin/echo 0 > /sys/class/pwm/pwmchip0/export");
-	}
-	if(access("/sys/class/pwm/pwmchip0/pwm1/enable",F_OK)!=0){
-		system("/bin/echo 1 > /sys/class/pwm/pwmchip0/export");
-	}
-	if(access("/sys/class/pwm/pwmchip2/pwm0/enable",F_OK)!=0){
-		system("/bin/echo 0 > /sys/class/pwm/pwmchip2/export");
-	}
-	if(access("/sys/class/pwm/pwmchip2/pwm1/enable",F_OK)!=0){
-		system("/bin/echo 1 > /sys/class/pwm/pwmchip2/export");
-	}
-	if(access("/sys/class/pwm/pwmchip4/pwm0/enable",F_OK)!=0){
-		system("/bin/echo 0 > /sys/class/pwm/pwmchip4/export");
-	}
-	if(access("/sys/class/pwm/pwmchip4/pwm1/enable",F_OK)!=0){
-		system("/bin/echo 1 > /sys/class/pwm/pwmchip4/export");
-	}
-
-	// set permissions
-	system("/bin/chown -R root:pwm /sys/class/pwm/pwmchip*/");
-	system("/bin/chmod -R ug+rw /sys/class/pwm/pwmchip*/");
 
 	return 0;
 }
